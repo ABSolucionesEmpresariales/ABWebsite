f485da98a8bb0b85b8d2047acba2e6bd9b1d64ef
Julian <julianlefunk@gmail.com>
2020-03-10 17:32:39 -0600

    configuraciones de imagen y footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,11 +2,9 @@ import React from "react"
 import Footer from "../components/footer"
 import Header from "../components/header"
 
-import '../styles/bootstrap-grid.min.css'
-
 const Layout = (props) => {
     return (
-        <div className='container-fluid'>
+        <div className='container-fluid mx-0'>
             <div>
                 <Header/>
                 {props.children}
