5429cbb4eb277b9f63f428189299f37d4314512d
Saul Garcia <sagus_97@hotmail.com>
2020-03-11 10:52:16 -0600

    Slider was Created

---
+++ src/content/slider-content.js
@@ -0,0 +1,29 @@
+export default [
+	{
+		title: 'Titulo del Slider',
+		description:
+			'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+		button: 'Read More',
+		image: 'https://i0.wp.com/www.transform-elearning.com/wp-content/uploads/2017/11/online-course-blog-header.jpg?w=1080&ssl=1',
+		user: 'Daniel',
+		userProfile: 'https://s7.postimg.cc/abavelo3v/1_3x.png',
+    },
+    {
+		title: 'Titulo de Otro Slider',
+		description:
+			'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+		button: 'Compañia',
+		image: 'https://rgprincipal.com/es/wp-content/uploads/2020/01/photo-1454165804606-c3d57bc86b40.jpg',
+		user: 'Augustus',
+		userProfile: 'https://s7.postimg.cc/abavelo3v/1_3x.png',
+    },
+    {
+		title: 'Tercer Slider',
+		description:
+			'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+		button: 'Aprende Dinero',
+		image: 'https://www.incimages.com/uploaded_files/image/970x450/getty_933383882_2000133420009280345_410292.jpg',
+		user: 'Mc Dinero',
+		userProfile: 'https://s7.postimg.cc/abavelo3v/1_3x.png',
+	}
+]
---
+++ src/pages/calendario_cursos.js
@@ -0,0 +1,49 @@
+import React from "react"
+import Layout from "../components/layout"
+import Head from '../components/head'
+import { Link, graphql, useStaticQuery } from 'gatsby'
+
+const CalendarioContenido = () => {
+
+    const data = useStaticQuery(graphql`
+    query MyQuery {
+        allContentfulCalendario {
+          edges {
+            node {
+              slug
+              imagenPrincipal {
+                file {
+                  fileName
+                  url
+                }
+              }
+              ubicacionCursos {
+                lat
+                lon
+              }
+              certificadoPor
+            }
+          }
+        }
+      }
+
+    `)
+
+    return (
+        <Layout>
+            <Head title="calendario_cursos"/>
+            <h1 className="text-center">Calendario de Cursos</h1>
+            <div className="row m-3 d-block d-lg-flex">
+                {data.CalendarioContenido.edges.map((edge) => {
+                  return (
+                    <Link to={"/calendario_cursos/"+edge.node.slug}>
+                      <img className="fluid" src={edge.node.imagenPrincipal.file} />
+                    </Link>
+                  )
+                })}
+            </div>
+        </Layout>
+    )
+}
+
+export default CalendarioContenido
