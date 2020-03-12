021b6ccaabbfd6d7e0c1bf24fa4b1b6e09de50d1
DE CM <DECM24>
2020-03-11 18:16:29 -0600

    catalogo-cursos-terminado

--- src/pages/cursos.js
+++ src/pages/cursos.js
@@ -24,34 +24,41 @@ const Cursos = () => {
         <Layout>
             <Head title="Cursos" />
             <div className="row d-block d-lg-flex">
-                <div className="col-3">
-                    jndjnd
+                <div className="col-lg-3 px-0 my-3">
+                    <div className="card text-center mx-lg-5">
+                        <h5 className="my-3">INFORMES DE CURSOS</h5>
+                        <p>¿Te interesa alguno de nuestros cursos?</p>
+                        <p>Déjanos tus datos y recibe más información.</p>
+                        <button className="btn btn-outline-dark mx-5 mb-4">Información</button>
+                    </div>
                 </div>
-                <div className='col-9 d-block d-lg-flex'>
-                    {mysql.allMysqlCursos.edges.map((edge) => {
-                        {
-                            const direccion = 'https://www.escuelaalreves.com/';
-                            let ruta = edge.node.imagen;
-                            ruta = ruta.split('/');
-                            const carpeta = ruta[1];
-                            const archivo = 'res_' + ruta[2];
-                            var url = direccion + carpeta + "/" + archivo;
-                        }
-                        return (
-                            <div className="card col-4">
-                                <div className="card-body text-center">
-                                    <img className="img-fluid" src={url}></img>
-                                    <h5 className="mt-2">{edge.node.nombre}</h5>
-                                    <div className="row d-block d-lg-flex justify-content-center ">
-                                        <button className="btn btn-success col-5 mx-1">Mostrar</button>
-                                        <button className="btn btn-primary col-5 mx-1 " disabled>${edge.node.costo} MX</button>
-                                    </div>
+                <div className="col-lg-9">
+                    <div className="row d-block d-lg-flex">
+                        {mysql.allMysqlCursos.edges.map((edge) => {
+                            {
+                                const direccion = 'https://www.escuelaalreves.com/';
+                                let ruta = edge.node.imagen;
+                                ruta = ruta.split('/');
+                                const carpeta = ruta[1];
+                                const archivo = 'res_' + ruta[2];
+                                var url = direccion + carpeta + "/" + archivo;
+                            }
+                            return (
+                                <div className="card col-lg-3 my-3 ml-lg-5">
+                                    <div className="card-body text-center">
+                                        <img className="img-fluid" src={url}></img>
+                                        <h5 className="mt-2">{edge.node.nombre}</h5>
+                                        <div className="row d-block d-lg-flex justify-content-center">
+                                            <a className="btn btn-success mx-1" href='https://www.escuelaalreves.com/#all-cursos'>Mostrar</a>
+                                            <button className="btn btn-primary mx-1 " disabled>${edge.node.costo}MX</button>
+                                        </div>
 
+                                    </div>
                                 </div>
-                            </div>
-                        )
-                    }
-                    )}
+                            )
+                        }
+                        )}
+                    </div>
                 </div>
             </div>
 
