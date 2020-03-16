9bc202af209ab75d66fd221b4e9accea4464a508
Saul Garcia <sagus_97@hotmail.com>
2020-03-16 10:34:24 -0600

    Creacion de componente paralax reutilizable, es un Banner, esta siendo utilizado en /pages/quienes-somos.js

--- src/pages/quienes-somos.js
+++ src/pages/quienes-somos.js
@@ -1,11 +1,43 @@
 import React from 'react'
+import Layout from '../components/layout'
+import Parallax from '../components/paralax'
+import QuienesCss from '../styles/quienesSomos.module.scss'
+
 
 const QuienesSomos = () => {
+
     return (
         <Layout>
-            <Head title="Quienes-somos" />
-            
+            <Parallax title='QUIENES SOMOS'
+                subtitle='Capacitamos pequeñas y grandes empresas'
+                classNames={QuienesCss} />
 
+            <div className="row">
+                <div className="col-sm-4">
+                    <h1>ARTIFICIAL BRAIN</h1>
+                    <p>
+                        Aliqua reprehenderit mollit aute nostrud irure dolore velit amet. Cillum nulla est Lorem
+                        irure aliquip velit exercitation et culpa deserunt eu irure cupidatat esse.
+                        Esse aliquip aute aute tempor qui consectetur dolore duis nulla nostrud. Ea sit occaecat
+                        occaecat qui nulla ipsum eiusmod laborum est exercitation pariatur excepteur reprehenderit.
+                    </p>
+                    <p>
+                        Aliqua reprehenderit mollit aute nostrud irure dolore velit amet. Cillum nulla est Lorem
+                        irure aliquip velit exercitation et culpa deserunt eu irure cupidatat esse.
+                        Esse aliquip aute aute tempor qui consectetur dolore duis nulla nostrud. Ea sit occaecat
+                        occaecat qui nulla ipsum eiusmod laborum est exercitation pariatur excepteur reprehenderit.
+                    </p>
+                </div>
+
+                <div className="col-sm-6">
+                    <div>
+                        
+                    </div>
+                </div>
+            </div>
         </Layout>
+
     )
 }
+
+export default QuienesSomos
