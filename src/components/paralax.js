e63e06f75aeb55c6fb030d51f744f7a6a0c15478
Uriel Cisneros <urielcisne96@gmail.com>
2020-03-17 12:10:33 -0600

    ServiciosFull

---
+++ src/components/paralax.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+class Parallax extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        
+        return (
+            <div className="row">
+                <div className={this.props.classNames.parallax + ' col-sm-12'}>
+                    <section className={this.props.classNames.center}>
+                        <h1>{this.props.title}</h1>
+                        <p>{this.props.subtitle}</p>
+                    </section>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Parallax
