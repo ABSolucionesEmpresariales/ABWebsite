07ff50604b7400a9b2eb494fd09d14030274cb05
AldoChagollan <aldochagollan@outlook.com>
2020-03-18 10:21:32 -0600

    warnings

--- src/components/header.js
+++ src/components/header.js
@@ -36,12 +36,7 @@ const Header = () => {
 
     return (
         <header className="row">
-            <link
-                rel="stylesheet"
-                href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-                crossorigin="anonymous"
-            />
+
             <Navbar expand="lg" className={'col-12 '+headerStyles.border}>
                 <Navbar.Brand to="/home">
                     <Link to="/"><Image src={data.fileName.childImageSharp.fluid.src} alt="ABsoluciones" fluid /></Link>
